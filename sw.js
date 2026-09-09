de6739e0c2325bfd434865789ab08478d6a3ea62
Abshar Dev <dev@abshar.app>
2026-09-09 00:02:57 +0000

    v7.2 final github fix

---
+++ sw.js
@@ -0,0 +1,22 @@
+
+const V='v72-1788794154';
+
+self.addEventListener('install', e => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request, { cache: 'no-store' })
+      .then(r => r)
+      .catch(() => caches.match(e.request))
+  );
+});
